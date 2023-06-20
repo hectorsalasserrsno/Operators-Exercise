7cf0d7bc6c5df0160324d41a353587ae429a6880
Hector Salas Serrano <kenkira666@gmail.com>
2023-06-20 14:19:25 -0500

    exercise 1 ready

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace OperatorExercise
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            
+            
+            int a = 17;
+            int b = 4;
+            int quotuent = a / b;
+            int remainder = a % b;
+
+
+
+            Console.WriteLine( a + b);
+            Console.WriteLine( a - b);
+            Console.WriteLine(a/b);
+            Console.WriteLine(a*b);
+
+            if(a == 17 &&  b == 4) 
+            { 
+                Console.WriteLine("17/4 is " + quotuent);
+                Console.WriteLine("reminder " + remainder);
+            }
+            
+            
         }
     }
 }
